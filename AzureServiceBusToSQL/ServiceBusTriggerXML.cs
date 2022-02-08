401e65a6e69f854c0b3c5fd0a42821bf1fb4bdd4
obinna <oigbokwe73@gmail.com>
2022-02-07 22:13:03 -0500

    Add project files.

---
+++ AzureServiceBusToSQL/ServiceBusTriggerXML.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Specialized;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Xenhey.BPM.Core;
+using Xenhey.BPM.Core.Implementation;
+
+namespace AzureServiceBusToSQL
+{
+    public static class ServiceBusTriggerXML
+    {
+        [FunctionName("ServiceBusTriggerXML")]
+        public static void Run([ServiceBusTrigger("mytopic", "mysubscription", Connection = "")] string mySbMsg, ILogger log)
+        {
+            string ApiKeyName = "x-api-key";
+            log.LogInformation("C# blob trigger function processed a request.");
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add(ApiKeyName, "43EFE991E8614CFB9EDECF1B0FDED37D");
+            IOrchrestatorService orchrestatorService = new ManagedOrchestratorService(nvc);
+            var processFiles = orchrestatorService.Run(mySbMsg);
+        }
+    }
+}
---
+++ AzureServiceBusToSQL/search.cs
@@ -0,0 +1,54 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Xenhey.BPM.Core.Implementation;
+using Xenhey.BPM.Core;
+using System.Collections.Specialized;
+using System.Linq;
+using System.IO;
+
+namespace AzureServiceBusToSQL
+{
+    public class Search
+    {
+        private HttpRequest _req;
+        private NameValueCollection nvc = new NameValueCollection();
+        [FunctionName("search")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            HttpRequest req, ILogger log)
+        {
+            _req = req;
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = await new StreamReader(_req.Body).ReadToEndAsync();
+            var results = orchrestatorService.Run(requestBody);
+            return resultSet(results);
+
+        }
+
+        private ActionResult resultSet(string reponsePayload)
+        {
+            var returnContent = new ContentResult();
+            var mediaSelectedtype = _req.Headers.Where(x => x.Key.Equals("Content-Type")).FirstOrDefault();
+            returnContent.Content = reponsePayload;
+            returnContent.ContentType = mediaSelectedtype.Value;
+            return returnContent;
+        }
+        private IOrchrestatorService orchrestatorService
+        {
+            get
+            {
+
+                _req.Headers.ToList().ForEach(item =>
+                {
+                    nvc.Add(item.Key, item.Value.FirstOrDefault());
+                });
+                return new ManagedOrchestratorService(nvc);
+            }
+        }
+
+    }
+}
