da10fd27db72ac6c9ab818c44a093c12ebde00ac
obinna <oigbokwe73@gmail.com>
2022-02-08 23:51:53 -0500

    updates to code

--- AzureServiceBusToSQL/ServiceBusTriggerJSON.cs
+++ AzureServiceBusToSQL/ServiceBusTriggerJSON.cs
@@ -11,12 +11,12 @@ namespace AzureServiceBusToSQL
     public static class ServiceBusTriggerJSON
     {
         [FunctionName("ServiceBusTriggerJSON")]
-        public static void Run([ServiceBusTrigger("jsonpayload", "training202202", Connection = "ServiceBusConnectionString")]string mySbMsg, Int32 deliveryCount,DateTime enqueuedTimeUtc, string messageId, ILogger log)
+        public static void Run([ServiceBusTrigger("jsonpayload", "messages", Connection = "ServiceBusConnectionString")]string mySbMsg, Int32 deliveryCount,DateTime enqueuedTimeUtc, string messageId, ILogger log)
         {
             string ApiKeyName = "x-api-key";
             log.LogInformation("C# blob trigger function processed a request.");
             NameValueCollection nvc = new NameValueCollection();
-            nvc.Add(ApiKeyName, "43EFE991E8614CFB9EDECF1B0FDED37B");
+            nvc.Add(ApiKeyName, "43EFE991E8614CFB9EDECF1B0FDED37D");
             IOrchrestatorService orchrestatorService = new ManagedOrchestratorService(nvc);
             var processFiles = orchrestatorService.Run(mySbMsg);
             log.LogInformation($"EnqueuedTimeUtc={enqueuedTimeUtc}");
--- AzureServiceBusToSQL/UploadFile.cs
+++ AzureServiceBusToSQL/UploadFile.cs
@@ -1,35 +1,51 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Xenhey.BPM.Core.Implementation;
+using Xenhey.BPM.Core;
+using System.Collections.Specialized;
+using System.Linq;
 
 namespace AzureServiceBusToSQL
 {
-    public static class UploadFile
+   public class UploadFile
     {
-        [FunctionName("UploadFile")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        private HttpRequest _req;
+        private NameValueCollection nvc = new NameValueCollection();
+        [FunctionName("uploadfile")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            HttpRequest req, ILogger log)
         {
+            _req = req;
+
             log.LogInformation("C# HTTP trigger function processed a request.");
+            var results = orchrestatorService.Run(_req.Body);
+            return resultSet(results);
 
-            string name = req.Query["name"];
+        }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+        private ActionResult resultSet(string reponsePayload)
+        {
+            var returnContent = new ContentResult();
+            var mediaSelectedtype = _req.Headers.Where(x => x.Key.Equals("Content-Type")).FirstOrDefault();
+            returnContent.Content = reponsePayload;
+            returnContent.ContentType = mediaSelectedtype.Value;
+            return returnContent;
+        }
+        private IOrchrestatorService orchrestatorService
+        {
+            get
+            {
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+                _req.Headers.ToList().ForEach(item =>
+                {
+                    nvc.Add(item.Key, item.Value.FirstOrDefault());
+                });
+                return new ManagedOrchestratorService(nvc);
+            }
         }
     }
-}
+    }
