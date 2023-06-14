c1f50012d6182b5f1ef4bf67d4aeaf0f5e8093bd
obinna <oigbokwe73@gmail.com>
2023-06-14 18:55:47 -0400

    Updates to include more config

---
+++ AzureServiceBusToSQL/reports.cs
@@ -0,0 +1,50 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Specialized;
+using System.Linq;
+using System.IO;
+using Xenhey.BPM.Core.Net6.Implementation;
+using Xenhey.BPM.Core.Net6;
+
+namespace AzureServiceBusToSQL
+{
+    public class reports
+    {
+        private HttpRequest _req;
+        private NameValueCollection nvc = new NameValueCollection();
+        [FunctionName("reports")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "report/{reportid}")]
+            HttpRequest req, string reportid, ILogger log)
+        {
+            var input = JsonConvert.SerializeObject(new { reportid });
+            _req = req;
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = input;
+            _req.Headers.ToList().ForEach(item => { nvc.Add(item.Key, item.Value.FirstOrDefault()); });
+            var results = orchrestatorService.Run(requestBody);
+            return resultSet(results);
+        }
+
+        private ActionResult resultSet(string reponsePayload)
+        {
+            var returnContent = new ContentResult();
+            var mediaSelectedtype = nvc.Get("Content-Type");
+            returnContent.Content = reponsePayload;
+            returnContent.ContentType = mediaSelectedtype;
+            return returnContent;
+        }
+        private IOrchestrationService orchrestatorService
+        {
+            get
+            {
+                return new ManagedOrchestratorService(nvc);
+            }
+        }
+
+    }
+}
