9946b82eb385cec4e2a93dea61176a37364c6d20
obinna <oigbokwe73@gmail.com>
2024-08-14 15:06:19 -0400

    updates

--- AzureServiceBusToSQL/Program.cs
+++ AzureServiceBusToSQL/Program.cs
@@ -1,7 +1,19 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
+     .ConfigureServices(services =>
+ {
+     services.AddApplicationInsightsTelemetryWorkerService();
+     services.ConfigureFunctionsApplicationInsights();
+     services.Configure<KestrelServerOptions>(options =>  
+    {  
+        options.AllowSynchronousIO = true;  
+    });  
+ })
     .Build();
 
 host.Run();
