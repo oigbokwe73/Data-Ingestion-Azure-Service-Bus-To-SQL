74fef22cef999c9495641eb31579528fd0a93e27
obinna <oigbokwe73@gmail.com>
2025-06-26 13:20:29 -0400

    updates

---
+++ Filedroptrigger.cs
@@ -0,0 +1,53 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+
+using System;
+using System.IO;
+using System.Text.Json;
+using Azure.Messaging;
+using Azure.Messaging.EventGrid;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Xenhey.BPM.Core.Net8.Implementation;
+using Xenhey.BPM.Core.Net8;
+using Microsoft.Graph.Models;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
+using Microsoft.Azure.Management.Sql.Fluent.Models;
+
+namespace AzureServiceBusToSQL;
+
+public class Filedroptrigger
+{
+    private readonly ILogger<Filedroptrigger> _logger;
+    private NameValueCollection nvc = new NameValueCollection();
+
+    public Filedroptrigger(ILogger<Filedroptrigger> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(Filedroptrigger))]
+    public void Run([EventGridTrigger] EventGridEvent eventGridEvent)
+    {
+        string ApiKeyName = "x-api-key";
+        _logger.LogInformation($"Received event: {eventGridEvent.EventType}");
+
+        if (eventGridEvent.EventType != "Microsoft.Storage.BlobCreated")
+        {
+            _logger.LogWarning("Event is not a BlobCreated event. Skipping.");
+            return;
+        }
+        NameValueCollection nvc = new NameValueCollection
+        {
+            { ApiKeyName, "2484F9382E974133A216F8E39BF4C389" }
+        };
+        var input = JsonConvert.SerializeObject(eventGridEvent);
+        IOrchestrationService orchrestatorService = new RemoteOrchrestratorService(nvc);
+        var processFiles = orchrestatorService.Run(input);
+
+        
+
+    }
+}
