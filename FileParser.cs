6cc6aead1dec346bef3585800934ba0ac8a8e4fe
obinna <oigbokwe73@gmail.com>
2025-06-29 14:14:47 -0400

    update

--- FileParser.cs
+++ FileParser.cs
@@ -1,32 +1,53 @@
-using System.Collections.Specialized;
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+
+using System;
 using System.IO;
-using Microsoft.Extensions.Logging;
-using Xenhey.BPM.Core.Net8;
-using Xenhey.BPM.Core.Net8.Implementation;
+using System.Text.Json;
+using Azure.Messaging;
+using Azure.Messaging.EventGrid;
+using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Xenhey.BPM.Core.Net8.Implementation;
+using Xenhey.BPM.Core.Net8;
+using Microsoft.Graph.Models;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
+using Microsoft.Azure.Management.Sql.Fluent.Models;
 
-namespace AzureServiceBusToSQL
+namespace AzureServiceBusToSQL;
+
+public class FileParser
 {
-    public class FileParser
+    private readonly ILogger<FileParser> _logger;
+    private NameValueCollection nvc = new NameValueCollection();
+
+    public FileParser(ILogger<FileParser> logger)
     {
-        private readonly ILogger _logger;
+        _logger = logger;
+    }
 
-        public FileParser(ILogger<FileParser> logger)
-        {
-            _logger = logger;
-        }
+    [Function(nameof(FileParser))]
+    public void Run([EventGridTrigger] EventGridEvent eventGridEvent)
+    {
+        string ApiKeyName = "x-api-key";
+        _logger.LogInformation($"Received event: {eventGridEvent.EventType}");
 
-        [Function("FileParser")]
-        public void Run([BlobTrigger("pickup/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob, string name)
+        if (eventGridEvent.EventType != "Microsoft.Storage.BlobCreated")
         {
-            string ApiKeyName = "x-api-key";
-            _logger.LogInformation("C# blob trigger function processed a request.");
-            NameValueCollection nvc = new NameValueCollection();
-            nvc.Add(ApiKeyName, "35C8400673BD4566AF97227BBC7A2754");
-            nvc.Add("ContainerName", name);
-            IOrchestrationService orchrestatorService = new RemoteOrchrestratorService(nvc);
-            var processFiles = orchrestatorService.Run(myBlob);
-            _logger.LogInformation(processFiles);
+            _logger.LogWarning("Event is not a BlobCreated event. Skipping.");
+            return;
         }
+        NameValueCollection nvc = new NameValueCollection
+        {
+            { ApiKeyName, "2484F9382E974133A216F8E39BF4C389" }
+        };
+        var input = JsonConvert.SerializeObject(eventGridEvent);
+        IOrchestrationService orchrestatorService = new RemoteOrchrestratorService(nvc);
+        var processFiles = orchrestatorService.Run(input);
+
+
+
     }
 }
