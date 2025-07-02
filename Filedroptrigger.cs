3e626d1e6e4b59c30a9ea192a47da10b60c323fa
obinna <oigbokwe73@gmail.com>
2025-07-02 16:35:50 -0400

    update package

--- Filedroptrigger.cs
+++ Filedroptrigger.cs
@@ -1,20 +1,13 @@
 // Default URL for triggering event grid function in the local environment.
 // http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 
-using System;
-using System.IO;
-using System.Text.Json;
-using Azure.Messaging;
 using Azure.Messaging.EventGrid;
-using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using Xenhey.BPM.Core.Net8.Implementation;
-using Xenhey.BPM.Core.Net8;
-using Microsoft.Graph.Models;
 using Newtonsoft.Json;
 using System.Collections.Specialized;
-using Microsoft.Azure.Management.Sql.Fluent.Models;
+using Xenhey.BPM.Core.Net8;
+using Xenhey.BPM.Core.Net8.Implementation;
 
 namespace AzureServiceBusToSQL;
 
@@ -35,7 +28,7 @@ public class Filedroptrigger
         _logger.LogInformation($"Received event: {eventGridEvent.EventType}");
         NameValueCollection nvc = new NameValueCollection
         {
-            { ApiKeyName, "2484F9382E974133A216F8E39BF4C389" }
+            { ApiKeyName, "35C8400673BD4566AF97227BBC7A2754" }
         };
         var input = JsonConvert.SerializeObject(eventGridEvent);
         IOrchestrationService orchrestatorService = new RemoteOrchrestratorService(nvc);
