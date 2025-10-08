e564294f96dc77bf24d5bdde96c4a66f19fea6fc
Obinna Igbokwe <oigbokwe73@gmail.com>
2025-10-08 09:08:00 -0400

    Update EventGridScript.ps1

--- DBScript/EventGridScript.ps1
+++ DBScript/EventGridScript.ps1
@@ -1,14 +1,29 @@
-$subscriptions = "4501a4d3-74c8-4703-9948-8c405a64daf0"
-$resourceGroups = "training20250806"
-$storageAccounts = "training20250806"
-$functionAppName = "training20250806"
+$subscriptions = ""
+$resourceGroups = "training20251008"
+$storageAccounts = "training20251008"
+$functionAppName = "training20251008"
 $function = "Filedroptrigger"
+$function1 = "FileParser"
 $containerName = "processed"
+$containerName1 = "pickup"
 az eventgrid event-subscription create `
-  --name blob-monitor-subscription `
+  --name blob-monitor-processed `
   --source-resource-id "/subscriptions/$subscriptions/resourceGroups/$resourceGroups/providers/Microsoft.Storage/storageAccounts/$storageAccounts" `
   --included-event-types Microsoft.Storage.BlobCreated  `
   --endpoint-type azurefunction `
   --endpoint "/subscriptions/$subscriptions/resourceGroups/$resourceGroups/providers/Microsoft.Web/sites/$functionAppName/functions/$function" `
-  --advanced-filter data.blobType StringContains BlockBlob `
-  --advanced-filter subject StringBeginsWith "/blobServices/default/containers/$containerName/"
+  --subject-begins-with "/blobServices/default/containers/$containerName/"  `
+  --subject-ends-with ".csv"  `
+  --max-delivery-attempts 1  `
+  --event-ttl 60  `
+
+
+  az eventgrid event-subscription create `
+  --name stp `
+  --source-resource-id "/subscriptions/$subscriptions/resourceGroups/$resourceGroups/providers/Microsoft.Storage/storageAccounts/$storageAccounts" `
+  --included-event-types Microsoft.Storage.BlobCreated  `
+  --endpoint-type azurefunction `
+  --endpoint "/subscriptions/$subscriptions/resourceGroups/$resourceGroups/providers/Microsoft.Web/sites/$functionAppName/functions/$function1" `
+  --subject-begins-with "/blobServices/default/containers/$containerName1/"  `
+  --subject-ends-with ".csv" --max-delivery-attempts 1
+  
